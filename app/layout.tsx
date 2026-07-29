939ffc3d9fc021fa2d2caa6e498235a01aea17a1
sibuh <misgandess@gmail.com>
2026-07-29 15:52:39 +0300

    added html tag to home page for gcp domain verification

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <meta name="google-site-verification" content="cUSGsRAfnBCAQhpzRtLUQmlGNf-F5UkTlsd3atLF4Ug" />
       </head>
       <body style={{ fontFamily: "'Inter', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif" }}>
         <Providers>
